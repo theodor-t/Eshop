df4ba61df42322dce9e36ca83bf23f1f25682c64
theodorerlax <sampjohn@mail.ru>
2021-11-25 18:39:31 +0200

    create design(Sign Up and Sign In)

--- app/src/main/java/com/example/eshop/SplashActivity.java
+++ app/src/main/java/com/example/eshop/SplashActivity.java
@@ -13,7 +13,7 @@ public class SplashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sphlash);
         SystemClock.sleep(3000);
-        Intent loginIntent = new Intent(SplashActivity.this,LoginActivity.class);
+        Intent loginIntent = new Intent(SplashActivity.this,RegisterActivity.class);
         startActivity(loginIntent);
         finish();
     }
