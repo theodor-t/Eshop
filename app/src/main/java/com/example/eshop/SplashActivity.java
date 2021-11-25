dcad66d249d4769a4dc9afd4481ffc0b51e61ff4
theodorerlax <sampjohn@mail.ru>
2021-11-25 16:42:22 +0200

    create Splashscreen

--- app/src/main/java/com/example/eshop/SplashActivity.java
+++ app/src/main/java/com/example/eshop/SplashActivity.java
@@ -2,7 +2,9 @@ package com.example.eshop;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.SystemClock;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sphlash);
+        SystemClock.sleep(3000);
+        Intent loginIntent = new Intent(SplashActivity.this,LoginActivity.class);
+        startActivity(loginIntent);
+        finish();
     }
 }
